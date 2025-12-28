b4a87a4a94ebc5110cf0fc4bcb56b14e87407aac
saketkumar28 <kumarsaket2801@gmail.com>
2025-12-28 11:49:44 +0530

    Backend Completed.

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,28 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const notifications = await Notification.find({ to: userId }).populate({
+      path: "from",
+      select: "username profileImg",
+    });
+
+    await Notification.updateMany({ to: userId }, { read: true });
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.log("error in getnotifications controller.");
+    res.status(500).json({ error: "Internal Server Error." });
+  }
+};
+
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    await Notification.deleteMany({ to: userId });
+    res.status(200).json({ message: "Notification deleted successfully." });
+  } catch (error) {
+    console.log("error in deleting notifications.", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server Running at : ${PORT}`);
